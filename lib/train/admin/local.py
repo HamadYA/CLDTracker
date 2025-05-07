58c753981838ab3d4c98ac575b30775f8a08b232
HamadYA <119171585+HamadYA@users.noreply.github.com>
2025-05-07 15:55:23 +0400

    Add files via upload

---
+++ lib/train/admin/local.py
@@ -0,0 +1,25 @@
+class EnvironmentSettings:
+    def __init__(self):
+        self.workspace_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker'    # Base directory for saving network checkpoints.
+        self.tensorboard_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/tensorboard'    # Directory for tensorboard files.
+        self.pretrained_networks = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/pretrained_networks'
+        self.lasot_dir = '/run/user/1000/gvfs/smb-share:server=192.168.1.1,share=vot/LaSOT/'
+        self.got10k_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/got10k/train'
+        self.got10k_val_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/got10k/val'
+        self.lasot_lmdb_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/lasot_lmdb'
+        self.got10k_lmdb_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/got10k_lmdb'
+        self.trackingnet_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/trackingnet'
+        self.trackingnet_lmdb_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/trackingnet_lmdb'
+        self.coco_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/coco'
+        self.coco_lmdb_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/coco_lmdb'
+        self.lvis_dir = ''
+        self.sbd_dir = ''
+        self.imagenet_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/vid'
+        self.imagenet_lmdb_dir = '/home/mohamadalansari/Desktop/CLDTrack/Train/CLDTracker/data/vid_lmdb'
+        self.imagenetdet_dir = ''
+        self.ecssd_dir = ''
+        self.hkuis_dir = ''
+        self.msra10k_dir = ''
+        self.davis_dir = ''
+        self.youtubevos_dir = ''
+        self.otb_dir = ''
