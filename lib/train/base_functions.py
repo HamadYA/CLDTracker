880cf16e00f02d36261e2cb83554e1361aeb7b31
HamadYA <119171585+HamadYA@users.noreply.github.com>
2025-05-19 07:56:01 +0400

    Add files via upload

--- lib/train/base_functions.py
+++ lib/train/base_functions.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data.distributed import DistributedSampler
 # datasets related
 from lib.train.dataset import Lasot, Got10k, MSCOCOSeq, ImagenetVID, TrackingNet
-from lib.train.dataset import Lasot_lmdb, Got10k_lmdb, MSCOCOSeq_lmdb, ImagenetVID_lmdb, TrackingNet_lmdb, NEW, TNL2K, OTB
+from lib.train.dataset import Lasot_lmdb, Got10k_lmdb, MSCOCOSeq_lmdb, ImagenetVID_lmdb, TrackingNet_lmdb, TNL2K, OTB, LaSOText
 from lib.train.data import sampler, opencv_loader, LTRLoader
 from lib.train.data import processing
 import lib.train.data.transforms as tfm
@@ -29,10 +29,8 @@ def names2datasets(name_list: list, settings, image_loader):
     assert isinstance(name_list, list)
     datasets = []
     for name in name_list:
-        # assert name in ["LASOT", "GOT10K_vottrain", "GOT10K_votval", "GOT10K_train_full", "GOT10K_official_val",
-        #                 "COCO17", "VID", "TRACKINGNET"]
         assert name in ["LASOT", "GOT10K_vottrain", "GOT10K_votval", "GOT10K_train_full", "GOT10K_official_val",
-                        "COCO17", "VID", "TRACKINGNET" , 'NEW', 'TNL2K', 'OTB']
+                        "COCO17", "VID", "TRACKINGNET", 'TNL2K', 'LASOTEXT', 'OTB']
         
         if name == "LASOT":
             if settings.use_lmdb:
@@ -83,14 +81,14 @@ def names2datasets(name_list: list, settings, image_loader):
             else:
                 # raise ValueError("NOW WE CAN ONLY USE TRACKINGNET FROM LMDB")
                 datasets.append(TrackingNet(settings.env.trackingnet_dir, image_loader=image_loader))
-
-        if name == "NEW":
-            datasets.append(NEW(settings.env.lasot_extension_subset_path, split='train', image_loader=image_loader))
         
 
         if name == "TNL2K":
             datasets.append(TNL2K(settings.env.tnl2k_dir, split='train', image_loader=image_loader))
-
+        
+        if name == "LASOTEXT":
+            datasets.append(LaSOText(settings.env.lasotext_dir, split='train', image_loader=image_loader))
+        
         if name == "OTB":
             datasets.append(OTB(settings.env.otb_dir, split='train', image_loader=image_loader))
 
